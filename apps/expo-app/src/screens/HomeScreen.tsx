31bef1847724032d27db065ca449d91b90b201a7
dominggo1999 <arnold.ds.dev@gmail.com>
2023-11-17 16:42:26 +0000

    test

--- apps/expo-app/src/screens/HomeScreen.tsx
+++ apps/expo-app/src/screens/HomeScreen.tsx
@@ -3,13 +3,19 @@ import tw from "twrnc";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import { RootStackParamList } from "./types";
 import { FC } from "react";
+import { Button } from "@/components";
+import { SparklesIcon } from "react-native-heroicons/solid";
 
 type Props = NativeStackScreenProps<RootStackParamList, "HomeScreen">;
 
 const HomeScreen: FC<Props> = ({ navigation }) => {
   return (
-    <View>
-      <Text>Home screen</Text>
+    <View style={tw`mt-10 flex items-center`}>
+      <Button onPress={() => navigation.goBack()}>Press me</Button>
+      <Text style={tw`mt-20`}>This is so cool</Text>
+      <View style={tw`mt-10 mx-auto`}>
+        <SparklesIcon style={tw`w-12 h-12 text-blue-500`} />
+      </View>
     </View>
   );
 };
